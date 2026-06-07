aba01290d9debce7e18d2afdaa5befcddaa281ec
freefire448768676 <freefire4487686767@gmail.com>
2026-06-07 15:30:51 +0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const { Telegraf } = require("telegraf");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+const { registerAdmin, registerAdminTextHandlers, startPingScheduler } = require("../workspace/admin.js");
+
+registerAdmin(bot);
+registerAdminTextHandlers(bot);
+startPingScheduler(bot);
+
+bot.launch().then(() => {
+  console.log("البوت اشتغل على Render ✅");
+});
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
